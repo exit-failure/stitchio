45091fa17d677c7191c495d99beb3825bc08e262
EXIT_FAILURE <hakrala@web.de>
2024-09-02 01:11:38 +0200

    - started with admin interface - removed temp files from version control

--- stitchio/filecrawler/models.py
+++ stitchio/filecrawler/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 
 # some common linux file systems only allow a file name length of 255 character
@@ -13,16 +14,16 @@ FILE_EXTENSION_LENGTH_LIMIT = 4
 PATH_LENGTH_LIMIT = 4096 - FILE_NAME_LENGTH_LIMIT - FILE_EXTENSION_LENGTH_LIMIT - 2
 
 class File_Extension(models.Model):
-	extension = models.CharField("extension", max_length=4)
+	extension = models.CharField(verbose_name="Extension", max_length=4)
 	extension.unique = True
 
 	def __str__(self):
 		return self.extension
 
 class Embroidery_Machine(models.Model):
-	manufacturer = models.CharField("manufacturer", max_length=30)
-	model = models.CharField("model", max_length=30)
-	info = models.CharField("info", max_length=100)
+	manufacturer = models.CharField(verbose_name="Manufacturer", max_length=30, help_text='The make of the machine. 30 characters max.')
+	model = models.CharField(verbose_name="Model", max_length=30, help_text='The model name of the machine. 30 characters max.')
+	info = models.CharField(verbose_name="Info", max_length=100, help_text='Additional information like the location, current status or identifier if you have multiple machines of the same make and model. 100 characters max.')
 	info.null = True
 	info.blank = True
 
@@ -30,9 +31,9 @@ class Embroidery_Machine(models.Model):
 		return '%s %s (%s)' % (self.manufacturer, self.model, self.info)
 
 class Embroidery_File(models.Model):
-	name = models.CharField("name", max_length=FILE_NAME_LENGTH_LIMIT)
+	name = models.CharField(verbose_name="Name", max_length=FILE_NAME_LENGTH_LIMIT)
 	ext = models.ForeignKey(File_Extension, on_delete=models.DO_NOTHING)
-	path = models.CharField("path", max_length=PATH_LENGTH_LIMIT)
+	path = models.CharField(verbose_name="Path", max_length=PATH_LENGTH_LIMIT)
 	on_machine = models.ForeignKey(Embroidery_Machine, on_delete=models.DO_NOTHING)
 	on_machine.null = True
 	on_machine.blank = True
